8fb7d2a4b3b9e69cf7c8049934eaff9df9eee3b0
cvcell <tropaguararia28@gmail.com>
2021-03-23 21:38:52 -0500

    configurando menu modificando estructura de datos

---
+++ js/AJmenu.js
@@ -0,0 +1,9 @@
+const cliente = document.getElementById("cliente")
+const venta = document.getElementById("venta")
+const stock = document.getElementById("stock")
+const color = "#ffffff"
+const color2 = "#5e9172"
+
+cliente.style.fill = color
+venta.style.fill = color
+stock.style.fill = color
