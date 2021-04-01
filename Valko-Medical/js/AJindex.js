429f70f68f0280d29feef02c8c55b41078a75402
cvcell <tropaguararia28@gmail.com>
2021-03-31 21:59:34 -0500

    agregado modo oscuro

--- Valko-Medical/js/AJindex.js
+++ Valko-Medical/js/AJindex.js
@@ -1,7 +1,9 @@
 import hamburgerMenu from "./AJmenu.js";
+import darkTheme from "./tema_oscuro.js";
 
 const d = document
 
 d.addEventListener("DOMContentLoaded", (e) =>{
     hamburgerMenu(".panel-btn", ".panel", ".menu a")
+    darkTheme(".dark-theme-btn", "dark-mode")
 })
