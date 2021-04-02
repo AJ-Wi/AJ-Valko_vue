f5ee871bd8c880e082cee864882acaed46021eb7
cvcell <tropaguararia28@gmail.com>
2021-04-02 18:43:48 -0500

    agregando variable localstorage

--- Valko-Medical/js/AJindex.js
+++ Valko-Medical/js/AJindex.js
@@ -2,8 +2,9 @@ import hamburgerMenu from "./AJmenu.js";
 import darkTheme from "./tema_oscuro.js";
 
 const d = document
-
+console.log(visualViewport)
 d.addEventListener("DOMContentLoaded", (e) =>{
     hamburgerMenu(".panel-btn", ".panel", ".menu a")
-    darkTheme(".dark-theme-btn", "dark-mode")
 })
+
+darkTheme(".dark-theme-btn", "dark-mode")
