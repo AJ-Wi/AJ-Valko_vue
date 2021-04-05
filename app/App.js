561f270d8b66414a15787177ae5dd68104cf08d0
cvcell <tropaguararia28@gmail.com>
2021-04-04 23:00:13 -0500

    renombrando archivos

--- app/App.js
+++ app/App.js
@@ -1,19 +1,22 @@
-import { inputAnimation } from "./components/inputs.js"
-import { menuBar, menuBtn } from "./components/menuBar.js"
-import { navBar, navBarBtn } from "./components/navBar.js"
-import { panelBar } from "./components/panelBar.js"
-import darkTheme from "./helpers/dark_Theme.js"
-import { recepcion } from "./views/recepcion.js"
+import { inputAnimation } from "./components/Inputs.js"
+import { menuBar, menuBtn } from "./components/MenuBar.js"
+import { navBar, navBarBtn } from "./components/NavBar.js"
+import { panelBar } from "./components/PanelBar.js"
+import darkTheme from "./helpers/Dark_Theme.js"
+import { Router } from "./router/router.js"
+import { body } from "./views/body.js"
 
 
 export function App(){
     const $app = document.getElementById("app")
 
+    $app.innerHTML = null    
     $app.appendChild(menuBar())
     $app.appendChild(panelBar())
-    $app.appendChild(recepcion())
+    $app.appendChild(body())
     $app.appendChild(navBar())  
     darkTheme(".dark-theme-btn", "dark-mode")
+    Router()
 }
 
 document.addEventListener("DOMContentLoaded", (e) =>{
