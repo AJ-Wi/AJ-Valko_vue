7af1a0652629967998b0d5b3b9f42a9cefaf3449
cvcell <tropaguararia28@gmail.com>
2021-04-06 22:50:13 -0500

    estructura

---
+++ app/helpers/vm_api.js
@@ -0,0 +1,8 @@
+const NAME = "valkomedical"
+const DOMAIN = `https://${NAME}.000webhostapp.com`
+
+
+export default {
+    NAME,
+    DOMAIN
+}
---
+++ app/views/login.js
@@ -0,0 +1,4 @@
+export function login(){
+    const main = document.createElement("main")
+    
+}
