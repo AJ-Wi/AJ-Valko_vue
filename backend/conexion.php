cf5cd39c630e8c03f40a6801de3fddb69c9fa015
cvcell <tropaguararia28@gmail.com>
2021-04-09 22:44:44 -0500

    creando redireciones

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,13 +1,13 @@
 <?php
 $host = 'localhost';
-$db = 'valkomedical';
+$db = 'apirest';//'apirestVM';
 $link = 'mysql:host='.$host.';dbname='.$db;
 $user = 'root';
 $pass = '';
 
 try {
     $pdo = new PDO($link, $user, $pass);
-	echo 'conectado';
+	//echo 'conectado';
 } catch (PDOExcepcion $e) {
     print "¡Error!: " . $e->getMessage() . "<br/>";
     die();
--- backend/vmApi.php
+++ backend/vmApi.php
@@ -1,9 +1,47 @@
 <?php
-$peticion = isset($_POST['peticion'])? $_POST['peticion'] : null;
+//header('Content-Type: application/json');
+$peticion = isset($_GET['peticion'])? $_GET['peticion'] : null;
+$pe = isset($_GET['p'])? $_GET['p'] : null;
 
-$datos = ['dolar'=>600, 'euro'=>700];
+$peticiones = [
+    'balones',
+    'usuarios',
+    'clientes',
+    'movimientos',
+    'estados',
+    'pacientes'
+];
 
+switch ($peticion){
+    case $peticiones[0]:
+        consulta($peticiones[0]);
+        break;
+    case $peticiones[1]:
+        consulta($peticiones[1]);
+        break;
+    case $peticiones[2]:
+        consulta($peticiones[2]);
+        break; 
+    case $peticiones[3]:
+        consulta($peticiones[3]);
+        break; 
+    case $peticiones[4]:
+        consulta($peticiones[4]);
+        break;
+    default:
+    echo '<h2>Peticion NO encontrada</h2><br>';
+    echo $peticion.'<br>';
+    echo $pe;
+}
 
-echo json_encode($datos);
+function consulta($peticion){
+    include_once 'conexion.php';
+
+    $sql = 'SELECT * FROM '.$peticion;
+    $sentencia = $pdo->prepare($sql);
+    $sentencia->execute();
+    $response = $sentencia->fetchAll();
+    echo $response;
+}
 
 ?>
