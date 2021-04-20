5c31f9dea5750c04e0086c618967a925a6130d0a
cvcell <tropaguararia28@gmail.com>
2021-04-20 14:34:02 -0500

    creando listas y botones

---
+++ src/components/Buttons.vue
@@ -0,0 +1,30 @@
+<template>
+  <input type="button" value="{{val}}">
+  <button type="button" class="btn">{{val}}</button>
+</template>
+
+<script>
+  export default{
+    name: "Buttons",
+    data(){
+      return{
+        val:this.value
+      }
+    },
+    props:["value"],
+    methods:{
+      enviar(){
+        //prevent default
+        
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .btn{
+    width: 40%;
+    height: 3rem;
+    background-color: #008afc;
+  }
+</style>
--- src/views/Enviar.vue
+++ src/views/Enviar.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="enviar">
     <h1>Esta es la pagina ENVIAR</h1>
+    <li>
+      <ol class="item">
+    <input type="checkbox" name="serial" id="serial" />
+    <label for="serial">123456</label>
+    <label for="nombre">nombre</label>
+    </ol>
+    </li>
   </div>
 </template>
 
 <script></script>
-<style scoped></style>
+<style scoped>
+  .item{
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
